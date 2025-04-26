0256da9a49a95fdb8d21d0e7a6408ee8897a4465
Madhumitha <madhumitha.subramaniam.5260@gmail.com>
2025-04-26 11:38:22 +0530

    New Files added, modified

---
+++ assets/script/main.js
@@ -0,0 +1,27 @@
+// /*******************
+//  * Animation on scroll
+//  ********************/
+
+// window.addEventListener('load', () => {
+//     AOS.init({
+//         duration: 1000,
+//         easing: "ease-in-out",
+//         once: true,
+//         mirror: true
+//     });
+// });
+
+
+/*******************
+ * Animation on scroll
+ ********************/
+
+window.addEventListener('load', () => {
+    AOS.init({
+        duration: 1000,
+        easing: "ease-in-out",
+        once: true,
+        mirror: true
+    });
+});
+
